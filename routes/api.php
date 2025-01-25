9ddf5e772892003868da257057697eb5d60c5157
david30-maker <davidokpah396@gmail.com>
2025-01-25 06:49:00 +0100

    add register routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get(uri: '/user', action: function (Request $request): mixed {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware(middleware: 'auth:sanctum');
+
+Route::post(uri: '/register', action: [AuthController::class, 'register']);
