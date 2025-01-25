58f3f25babcbebcf3982cb91a2ea99f68358fa96
david30-maker <davidokpah396@gmail.com>
2025-01-25 06:40:29 +0100

    add regidter method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register(Request $request) {
+    public function register(Request $request): JsonResponse|Response {
         $validated = $request->validate(rules: [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
