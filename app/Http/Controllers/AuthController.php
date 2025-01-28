646e1a9dcae5208dc325f42f575779846a2183fe
david30-maker <davidokpah396@gmail.com>
2025-01-28 21:59:42 +0100

    add

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    public function register(Request $request): JsonResponse|Response {
+    public function register(Request $request): \Illuminate\Http\JsonResponse{
         $validated = $request->validate(rules: [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
